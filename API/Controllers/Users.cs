d40bb794e1b430cbfaa394d35752b174dfc9047b
rohit <rohit.khurmi095@gmail.com>
2021-10-18 22:27:32 +0530

    EntityFramework Setup  (UserController)

---
+++ API/Controllers/Users.cs
@@ -0,0 +1,47 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Users : ControllerBase
+    {
+
+        //--------------------------------------
+        //DataContext object -> To Query Database
+        //--------------------------------------
+        private readonly DataContext _context;
+        public Users(DataContext context)
+        {
+            _context = context;
+        }
+
+        //_______________
+        // GET All Users
+        //_______________
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+
+        //________________
+        // GET User by Id
+        //________________
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int Id)
+        {
+            return await _context.Users.FindAsync(Id);
+        }
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+
+    //DbContext: for querying Database
+    //pass DbContext options -> DbContext
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        //==========
+        // DataSets
+        //==========
+        //Tables in Database
+        public DbSet<User> Users { get; set; }
+    }
+}
